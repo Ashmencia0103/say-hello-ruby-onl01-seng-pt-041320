ad5e6f18c185b9916b43a1d344b92e637876b747
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-15 23:59:06 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,2 +1,5 @@
-# I call on the method, say_hello.rb, a
+# I call on the method, say_hello.rb, and give it the string ("name")
+say_hello("Lisa!")
 
+# this method prints this text to the screen:
+puts Hello Lisa!
