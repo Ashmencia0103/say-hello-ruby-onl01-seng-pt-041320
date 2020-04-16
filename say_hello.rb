fd794bfee9f2b72d13c0906970186948056cccac
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-16 00:29:59 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
 # I call on the method, say_hello, and give it the string called "name" and a default value called "Ruby Programmer"
 def say_hello(name = "Ruby Programmer")
-   "Hello, #{name}!"
+  puts "Hello, #{name}!"
 end
